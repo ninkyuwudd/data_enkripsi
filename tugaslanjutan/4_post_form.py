0e356431275eb1ac1d4a5236d46369bc98454dfe
NinKyuWudd <116327302+ninkyuwudd@users.noreply.github.com>
2023-06-07 18:31:17 +0700

    change to sub,shif,chiper

--- tugaslanjutan/4_post_form.py
+++ tugaslanjutan/4_post_form.py
@@ -29,88 +29,47 @@ def get_db_connection():
                             password='beginer1383')
     return conn
 
+def generate_key():
+    # Menghasilkan kunci acak dengan mengacak urutan karakter alfabet
+    import random
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    alphabet_list = list(alphabet)
+    random.shuffle(alphabet_list)
+    key = ''.join(alphabet_list)
+    return key
 
+def encryptsubti(plaintext, key):
+    # Mengenkripsi teks menggunakan metode substitusi
+    ciphertext = ""
+    for char in plaintext:
+        if char.isalpha():
+            index = ord(char.lower()) - ord('a')
+            encrypted_char = key[index]
+            if char.isupper():
+                encrypted_char = encrypted_char.upper()
+            ciphertext += encrypted_char
+        else:
+            ciphertext += char
+    return ciphertext
 
-def kreate_key():
-    p1 = sympy.randprime(2 ** 4, 2 ** 8)
-    p2 = sympy.randprime(2 ** 4, 2 ** 8)
-    n = p1 * p2
-
-    print("N =", n)
-
-    pn = (p1 - 1) * (p2 - 1)
-    e = sympy.randprime(1, pn)
-    while math.gcd(e, pn) > 1:
-        e = sympy.randprime(1, pn)
-
-    print("Public key =", e)
-
-
-    d = (pn + 1) / e
-    i = 1
-
-    while d % 1 > 0:
-        i += 1
-        d = (i * pn + 1) / e
-    d = int(d)
-
-    print("privat key",d)
-    return [n,e,d]
-    
-	
-
-enkripsitext = ""
-def enkripsi(n,p,plain):
-    l = math.ceil(math.log2(n))
-
-    plain_byte = bytearray(plain,"utf-8")
-    chiper_binary = ""
-    for i in plain_byte:
-        i = i ** p % n
-        chiper_binary += format(i,"0" + str(l) + "b")
-
-    while len(chiper_binary) % 8 > 0:
-        chiper_binary += "0"
-
-        #format ke binary ke base64
-    chiper_byte = bytearray()
-    for x in range(0, len(chiper_binary), 8):
-        tmp = chiper_binary[x:x + 8]
-        tmp = int(tmp, 2)
-        chiper_byte.append(tmp)
-
-    chiper_base64 = base64.b64encode(chiper_byte)
-
-    return chiper_base64.decode("ascii")
-    # print("enkripsi message", chiper_base64.decode("ascii"))
-	
-	
+def decryptsubti(ciphertext, key):
+    # Mendekripsi teks menggunakan metode substitusi
+    plaintext = ""
+    for char in ciphertext:
+        if char.isalpha():
+            index = key.index(char.lower())
+            decrypted_char = chr(index + ord('a'))
+            if char.isupper():
+                decrypted_char = decrypted_char.upper()
+            plaintext += decrypted_char
+        else:
+            plaintext += char
+    return plaintext
 
 
 
-def dekripsi(n,p,chp):
-    l = math.ceil(math.log2(n))
 
-    chiper = chp.encode("ascii")
-    chiper_byte = base64.b64decode(chiper)
-    chiper_binary = ""
-
-    for x in chiper_byte:
-        chiper_binary += format(x,"08b")
-
-    #dekirpsi
-    plain = ""
-    for x in range(0, len(chiper_binary),l):
-        x = chiper_binary[x:x + l]
-        x = int(x,2)
-        plain += chr(x ** p % n)
-
-    return plain
-    # print("plain text: ",plain)
-
-	
-
-def encryptshift(text, shift):
+def encrypt(text, shift):
     encrypted_text = ""
     for char in text:
         if char.isalpha():
@@ -121,8 +80,7 @@ def encryptshift(text, shift):
             encrypted_text += char
     return encrypted_text
 
-
-def decryptshift(text, shift):
+def decrypt(text, shift):
     decrypted_text = ""
     for char in text:
         if char.isalpha():
@@ -132,6 +90,20 @@ def decryptshift(text, shift):
         else:
             decrypted_text += char
     return decrypted_text
+
+
+def enkripsibasic(inputtext):
+    res = ""
+    for i in range(len(inputtext)):
+        res += chr((ord(inputtext[i])-32+3)%95+32)
+    return res
+
+def decriptbasic(inputchiper):
+    res2 = ""
+    for i in range(len(inputchiper)):
+        res2 += chr((ord(inputchiper[i])-32-3)%95+32)
+    return res2
+
 
 
 
@@ -187,7 +159,7 @@ def login():
 	clear_n = ndata[0]
 	# clear_pub = ndata[0]
 
-	create_dekrip_data = decryptshift(dekripsi(clear_n,int(key),cleaned_text),3)
+	create_dekrip_data = (decrypt(decryptsubti(decriptbasic(cleaned_text),key), 3))
 
 	print(cleaned_text)
 	print(clear_n)
@@ -215,7 +187,7 @@ def login():
 		access_token = create_access_token(identity=username)
 		print('access_token: ', access_token),
 		response = make_response("logged in success")
-		kreate_key()
+		# kreate_key()
 		response.set_cookie('access_token',value=access_token,httponly=True)
 		return jsonify({"msg": access_token, 'berhasil':1}), 200
 	print('Failed...')
@@ -232,18 +204,18 @@ def register():
 	print('password: ', password)
 	
 
-	kunci = kreate_key()
-	create_enrkip_data = enkripsi(kunci[0],kunci[1],encryptshift(password,3))
+	kunci = generate_key()
+	create_enrkip_data = enkripsibasic(encryptsubti(encrypt(password, 3),kunci))
 	
 
 	conn = get_db_connection()
 	cur = conn.cursor()
-	strQuery = "INSERT INTO datauser (username,password,n_num) VALUES ('%s','%s',%s)" % (username,create_enrkip_data,kunci[0])
+	strQuery = "INSERT INTO datauser (username,password,n_num) VALUES ('%s','%s',%s)" % (username,create_enrkip_data,1)
 	# print(dekripsi(kunci[0],kunci[2],enkripsi(kunci[0],kunci[1],password)))
 	cur.execute(strQuery)
 	conn.commit()
 
-	return jsonify({"msg":kunci[2]})
+	return jsonify({"msg":kunci})
 		
 	
 	
